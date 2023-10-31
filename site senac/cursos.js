3084053204f857709f5c6b4329e37b2e8a567cbc
gabriel <143848942+Baki5129@users.noreply.github.com>
2023-10-30 21:43:37 -0300

    Add files via upload

---
+++ site senac/cursos.js
@@ -0,0 +1,3 @@
+var texto= document.getElementById ("verde")
+texto.style.backgroundColor= "green"
+texto.style.color= "black"
---
+++ site senac/index.js
@@ -0,0 +1,132 @@
+//var nome = "Gabriel";
+//var mensagem = " Seja bem vindo ao site " + nome;
+//alert(mensagem)
+//var nome=prompt ("Qua e o seu nome")
+//alert(`Seja bem vindo ao site do senac ${nome}`)
+/*alert( "seja bem vindo au Senac")
+var nome=prompt ("Qua e o seu nome")
+var nota=prompt ("Qua e o seu nota")
+if (nota <60) {
+    alert("Aluno "+ nome + " Reprovado " + nota + "%"  )
+}
+else if (nota >=60) {
+    alert("Aluno" + nome +" Aprovado "  + nota + "%"  )
+} */
+
+/*var idade = prompt("Qua e o seu Idade")
+
+if (idade >= 18) {
+    var curso = prompt("Qua e o seu curso")
+    alert("Pessoa maior de idade. Crusando" + curso)
+}
+
+else {
+    alert("Pessoa menor de idade. Não pode estar cursando na facudade")
+}*/
+/*var pesso = prompt("Qua o seu pesso")//captulando o pesso
+var altura = prompt("Qua a sua atura")//captulando o altura 
+var imc = (pesso / (altura * altura)).toFixed(1);
+alert("se Imc e." + imc)
+if (imc < 18.5) {
+    alert("Calsificação: Magreza")
+
+}
+
+else if (imc >= 18.5 && imc <= 24.8) {
+    alert("Calsificação: Mormal")
+}
+else if (imc >= 25 && imc <= 29.9) {
+    alert("Calsificação: Sobrepesso")
+}
+else if (imc >= 30 && imc <= 29.9) {
+    alert("Calsificação: Obesidade")
+}
+else if (imc >= 40) {
+    alert("Calsificação: Obesidade Grave")
+}*/
+/*var listaDeDesenho=["Baki","Dragon ball","Naruto" ]
+
+console.log("Meu desenho favorito e " + listaDeDesenho[0]+" e "+listaDeDesenho[1]);
+listaDeDesenho.push("Yugioh")*/
+/*var carro={
+    modelo: "palio",
+    ano: "2008",
+    marca:"fiat",
+    cor:"prata"
+
+}
+
+var listaDeAlunos = ["Gabriel","Michael ", "Dyego", "jose"]
+var curso = {
+    texto: "Programador de Sistemas",
+    hora: 1200,
+    materias: [ "html", "css", "javascrit"], 
+    alunos: listaDeAlunos
+    
+}
+console.log("Lita de Alunos  " + curso.alunos + " do crusando o curso de " + curso.materias );
+curso.alunos.forEach(aluno=> console.log(aluno));*/
+
+/*function  somavalores(num1, num2){
+   console.log(num1 + num2)
+} 
+
+somavalores(10, 20 )
+somavalores(20, 20 )
+somavalores(30, 20 )
+somavalores(40, 20 )
+somavalores(50, 20 )
+somavalores(60, 20 )*/
+
+/*function formataNome( nome,sobrenome){
+   console.log("Nome: " +nome +" "+sobrenome)
+
+}
+formataNome("gabriel","reis")*/
+
+
+/*-function formataCPF(cpf){
+    console.log(cpf[0] + cpf[1] + cpf[2]+"." + cpf[3] + cpf[4] + cpf[5] + "." + cpf[6] + cpf[7] + cpf[8] + "." + cpf[9] + cpf[10]  )
+} 
+ formataCPF("01782457631")
+ formataCPF("01782457631")
+ formataCPF("01782457631")
+ formataCPF("01782457631")
+ formataCPF("01782457631")*/
+
+/*function formataCPF(cpf){
+   console.log(cpf.slice(0, 3) + "." + cpf.slice(3, 6) + "." + cpf.slice(6, 9) + "-" + cpf.slice(9, 11))
+}
+formataCPF("01782457631")*/
+/*function formatasDados( nome, idade){
+    console.log( "A pessos " + nome + " tem " + idade + " anos " )
+}
+formatasDados("Gabriel",28)
+formatasDados("Michael",27)
+formatasDados("Jose",17)
+
+function multiplicar(num1, num2) {
+    var resultado = num1 * num2
+    console.log( resultado)
+}
+
+multiplicar(0, 2)
+multiplicar(1, 2)
+multiplicar(2, 2)
+multiplicar(3, 2)
+multiplicar(4, 2)
+multiplicar(5, 2)
+multiplicar(6, 2)
+multiplicar(7, 2)
+multiplicar(8, 2)
+multiplicar(9, 2)
+multiplicar(10, 2)*/
+
+/*var numero = prompt("numero ?")
+if (numero<10) {
+    alert ("menor que 10")
+}
+else if (numero =>10) {
+    alert ("maior que 10")
+}*/
+
